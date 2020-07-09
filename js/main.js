7f84c31cee5abd538b9d16efa5099a88b5bb216e
coderealistics <coderealistics@yandex.com>
2020-07-09 08:32:51 +0500

    Full update

--- js/main.js
+++ js/main.js
@@ -1,25 +1,21 @@
-const ready = function (cb) {
-    // Check if the `document` is loaded completely
-    document.readyState === "loading"
-    ? document.addEventListener("DOMContentLoaded", function (e) {
-        cb();
-    })
-    : cb();
+var ready = function (cb) {
+  // Check if the `document` is loaded completely
+  document.readyState === "loading"
+  ? document.addEventListener("DOMContentLoaded", function (e) {
+    cb();
+  })
+  : cb();
 };
 
 // Usage
 ready(function() {
-    
-    // Toggle sidenav on site-header__toggler click
-    var elSideHeaderToggler = document.querySelector('.site-header__toggler');
-    var elSidenav = document.querySelector('.sitenav');
-    elSideHeaderToggler.addEventListener('click', function(){
-        elSidenav.classList.toggle('sitenav--open');
-    });
-
-    elSidenav.addEventListener('animationend', function(){
-        console.log('tugadi');
-        
-    })
-
+  
+  // Toggle sitenav on site-header__toggler click
+  var elSiteHeaderToggler = document.querySelector('.site-header__toggler');
+  var elSitenav = document.querySelector('.sitenav');
+  
+  elSiteHeaderToggler.addEventListener('click', function () {
+    elSitenav.classList.toggle('sitenav--open');
+  });
+  
 });
